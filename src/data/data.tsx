9997a875d3e07472b1e6d01150d008a5d76d6291
Ang Norman <normanang@techtants.com>
2023-01-03 11:02:59 +0800

    changes

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -11,7 +11,6 @@ import {
 import GithubIcon from '../components/Icon/GithubIcon';
 import InstagramIcon from '../components/Icon/InstagramIcon';
 import LinkedInIcon from '../components/Icon/LinkedInIcon';
-import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
 import TwitterIcon from '../components/Icon/TwitterIcon';
 import heroImage from '../images/header-background.webp';
 import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
@@ -25,7 +24,7 @@ import porfolioImage8 from '../images/portfolio/portfolio-8.jpg';
 import porfolioImage9 from '../images/portfolio/portfolio-9.jpg';
 import porfolioImage10 from '../images/portfolio/portfolio-10.jpg';
 import porfolioImage11 from '../images/portfolio/portfolio-11.jpg';
-import profilepic from '../images/profilepic.jpg';
+import profilepic from '../images/profilepic.jpeg';
 import testimonialImage from '../images/testimonial.webp';
 import {
   About,
@@ -44,8 +43,8 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
-  description: "Example site built with Tim Baker's react resume template",
+  title: 'My Resume',
+  description: "Empowered by Nextjs-Vercel",
 };
 
 /**
@@ -73,14 +72,15 @@ export const heroData: Hero = {
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
-        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
-        registrar and site builder.
+        I'm a Malaysia based <strong className="text-stone-100">Full Stack Web Developer</strong>, currently working
+        at <strong className="text-stone-100">Techtants</strong> helping build a modern, mobile-first, innovative, decentralized
+        web applications.
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
-        plucking my <strong className="text-stone-100">banjo</strong>, or exploring beautiful{' '}
-        <strong className="text-stone-100">Vancouver Island</strong>.
+        Currently focusing on <strong className="text-stone-100">web3</strong>,
+        enroll on a <strong className="text-stone-100"> CS50 Harvard course</strong>,
+        building up my <strong className="text-stone-100">social networks</strong>, and exploring opportunities outside of my 
+        <strong className="text-stone-100"> home country</strong>.
       </p>
     </>
   ),
@@ -104,16 +104,15 @@ export const heroData: Hero = {
  */
 export const aboutData: About = {
   profileImageSrc: profilepic,
-  description: `Use this bio section as your way of describing yourself and saying what you do, what technologies you like
-  to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
-  in.`,
+  description: `Just a person who enjoys leisure activities like watching movies and playing games, passionate about programming, 
+  a bit of a laid-back personality and appreciate good food and relaxing`,
   aboutItems: [
-    {label: 'Location', text: 'Victoria, BC', Icon: MapIcon},
-    {label: 'Age', text: '29', Icon: CalendarIcon},
-    {label: 'Nationality', text: 'Canadian / Irish', Icon: FlagIcon},
-    {label: 'Interests', text: 'Motorcycles, Muay Thai, Banjos', Icon: SparklesIcon},
-    {label: 'Study', text: 'University of Victoria', Icon: AcademicCapIcon},
-    {label: 'Employment', text: 'Instant Domains, inc.', Icon: OfficeBuildingIcon},
+    {label: 'Location', text: 'Penang, Malaysia', Icon: MapIcon},
+    {label: 'Age', text: '26', Icon: CalendarIcon},
+    {label: 'Nationality', text: 'Malaysian / Chinese', Icon: FlagIcon},
+    {label: 'Interests', text: 'Gamer / Watching movies / Badminton', Icon: SparklesIcon},
+    {label: 'Study', text: 'University of Lincoln, UK', Icon: AcademicCapIcon},
+    {label: 'Employment', text: 'Techtants Sdn Bhd', Icon: OfficeBuildingIcon},
   ],
 };
 
@@ -126,15 +125,15 @@ export const skills: SkillGroup[] = [
     skills: [
       {
         name: 'English',
+        level: 8,
+      },
+      {
+        name: 'Chinese',
         level: 10,
       },
       {
-        name: 'French',
-        level: 4,
-      },
-      {
-        name: 'Spanish',
-        level: 3,
+        name: 'Malay',
+        level: 6,
       },
     ],
   },
@@ -142,15 +141,15 @@ export const skills: SkillGroup[] = [
     name: 'Frontend development',
     skills: [
       {
-        name: 'React',
-        level: 9,
+        name: 'ReactJs /  Nextjs',
+        level: 6,
       },
       {
         name: 'Typescript',
         level: 7,
       },
       {
-        name: 'GraphQL',
+        name: 'Vue.js',
         level: 6,
       },
     ],
@@ -160,14 +159,14 @@ export const skills: SkillGroup[] = [
     skills: [
       {
         name: 'Node.js',
+        level: 7,
+      },
+      {
+        name: 'PHP laravel',
         level: 8,
       },
       {
-        name: 'Rust',
-        level: 5,
-      },
-      {
-        name: 'Golang',
+        name: 'Python',
         level: 4,
       },
     ],
@@ -176,16 +175,8 @@ export const skills: SkillGroup[] = [
     name: 'Mobile development',
     skills: [
       {
-        name: 'React Native',
-        level: 9,
-      },
-      {
         name: 'Flutter',
         level: 4,
-      },
-      {
-        name: 'Swift',
-        level: 3,
       },
     ],
   },
@@ -268,39 +259,45 @@ export const portfolioItems: PortfolioItem[] = [
  */
 export const education: TimelineItem[] = [
   {
-    date: 'April 2007',
-    location: 'Clown college',
-    title: 'Masters in Beer tasting',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    date: 'March 2018',
+    location: 'University of Lincoln, UK',
+    title: 'Bachelors Degree of Computer Science',
+    content: <p>Strengthen my fundamental concepts and techniques in computer science, also the skills needed to apply these concepts to real-world problems.</p>,
   },
   {
-    date: 'March 2003',
-    location: 'School of Business',
-    title: 'What did you study 101',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
-  },
+    date: 'March 2015',
+    location: 'UOW KDU College',
+    title: 'Diploma in Computer Science',
+    content: <p>Learning the fundamental concepts and techniques in computer science, including programming, data structures, algorithms, computer architecture, and computer systems. Some programs may also cover topics in related fields such as mathematics and statistics, 
+    and may include practical experiences such as internships or capstone projects.</p>,
+  }
 ];
 
 export const experience: TimelineItem[] = [
   {
-    date: 'March 2010 - Present',
-    location: 'Awesome Development Company',
-    title: 'Senior UX Engineer',
+    date: 'September 2020 - Present',
+    location: 'Techtants',
+    title: 'Full stack web developer',
     content: (
-      <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+      <p>     
+        <li>Developed multiple projects using PHP Laravel, Vue.js, web3js, Solidity, Adonisjs, Reactjs/Nextjs.</li>
+        <li>Integrating back-end API modules to front-end web development </li>
+        <li>Designing the fundamental structure of smart contracts based on various standards such as ERC20, ERC721, and ERC1155 using Solidity</li>
+        <li>Collaborating with cross-functional teams to define, design, and ship new features</li>
+        <li>Staying up-to-date with the latest developments in the field and incorporating new technologies as appropriate</li>
+        <li>Participate in testing functionalities of dApps and also fixing defects in front-end / back-end codes.</li>
+        <li>Building multiple decentralized Applications powered by Binance Smart Chain & Polygon</li>
       </p>
+
     ),
   },
   {
-    date: 'March 2007 - February 2010',
-    location: 'Garage Startup Studio',
-    title: 'Junior bug fixer',
+    date: 'March 2017- September 2020',
+    location: 'Max-Groups Marine',
+    title: 'Junior IT Support interns',
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+        Building website powered by WordPress, learning about PHP web applications plug-ins. Solving website bugs if necessary.
       </p>
     ),
   },
@@ -313,13 +310,13 @@ export const testimonial: TestimonialSection = {
   imageSrc: testimonialImage,
   testimonials: [
     {
-      name: 'John Doe',
-      text: 'Use this as an opportunity to promote what it is like to work with you. High value testimonials include ones from current or past co-workers, managers, or from happy clients.',
+      name: 'Kent Beck',
+      text: 'I am not a great programmer; I am just a good programmer with great habits.',
       image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/169.jpg',
     },
     {
-      name: 'Jane Doe',
-      text: 'Here you should write some nice things that someone has said about you. Encourage them to be specific and include important details (notes about a project you were on together, impressive quality produced, etc).',
+      name: 'Freeman Dyson',
+      text: 'A good engineer is a person who makes a design that works with as few original ideas as possible',
       image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/14.jpg',
     },
     {
@@ -336,27 +333,27 @@ export const testimonial: TestimonialSection = {
 
 export const contact: ContactSection = {
   headerText: 'Get in touch.',
-  description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
+  description: 'Reach out to me via these for enquiries :)',
   items: [
     {
       type: ContactType.Email,
-      text: 'reachout@timbaker.me',
-      href: 'mailto:reachout@timbaker.me',
+      text: 'norman1997.an@gmail.com',
+      href: 'mailto:norman1997.an@gmail.com',
     },
     {
       type: ContactType.Location,
-      text: 'Victoria BC, Canada',
-      href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
+      text: 'Penang, Malaysia',
+      href: 'https://goo.gl/maps/M7hrCRLbKA1sXEjY6',
     },
     {
-      type: ContactType.Instagram,
+      type: ContactType.Twitter,
       text: '@tbakerx',
-      href: 'https://www.instagram.com/tbakerx/',
+      href: 'https://twitter.com/NormanAng97',
     },
     {
       type: ContactType.Github,
       text: 'tbakerx',
-      href: 'https://github.com/tbakerx',
+      href: 'https://github.com/Angangang97',
     },
   ],
 };
@@ -365,9 +362,8 @@ export const contact: ContactSection = {
  * Social items
  */
 export const socialLinks: Social[] = [
-  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/tbakerx'},
-  {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
-  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/timbakerx/'},
-  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/tbakerx/'},
-  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx'},
+  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/Angangang97'},
+  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/ang-norman-4b9593aa/'},
+  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/ang_norman'},
+  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/NormanAng97'},
 ];
